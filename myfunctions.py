bd128f3000a526e4c4c74166b520ec49b8c354f4
DmitriChe <juneority@mail.ru>
2019-10-17 21:11:52 +0300

    first commit

--- myfunctions.py
+++ myfunctions.py
@@ -12,7 +12,7 @@ def simple_separator():
     Функция создает красивый резделитель из 10-и звездочек (**********)
     :return: **********
     """
-    pass
+    return '*' * 10
 
 
 print(simple_separator() == '**********')  # True
@@ -24,21 +24,21 @@ def long_separator(count):
     :param count: количество звездочек
     :return: строка разделитель, примеры использования ниже
     """
-    pass
+    return '*' * count
 
 
 print(long_separator(3) == '***')  # True
 print(long_separator(4) == '****')  # True
 
 
-def separator(simbol, count):
+def separator(symbol, count):
     """
     Функция создает разделитель из любых символов любого количества
-    :param simbol: символ разделителя
+    :param symbol: символ разделителя
     :param count: количество повторений
     :return: строка разделитель примеры использования ниже
     """
-    pass
+    return str(symbol) * count
 
 
 print(separator('-', 10) == '----------')  # True
@@ -55,7 +55,7 @@ def hello_world():
     ##########
     :return: None
     """
-    pass
+    print('*' * 10 + '\n\n' + 'Hello World!' + '\n\n' + '#' * 8)
 
 
 '''
@@ -79,7 +79,7 @@ def hello_who(who='World'):
     :param who: кого мы приветствуем, по умолчанию World
     :return: None
     """
-    pass
+    print('*' * 10 + '\n\n' + f'Hello {who}!' + '\n\n' + '#' * 8)
 
 
 '''
@@ -115,7 +115,7 @@ def pow_many(power, *args):
     :param args: любое количество цифр
     :return: результат вычисления # True -> (1 + 2)**1
     """
-    pass
+    return sum(args) ** power
 
 
 print(pow_many(1, 1, 2) == 3)  # True -> (1 + 2)**1 == 3
@@ -133,7 +133,8 @@ def print_key_val(**kwargs):
     :param kwargs: любое количество именованных параметров
     :return: None
     """
-    pass
+    for key, val in kwargs.items():
+        print(f'{key} --> {val}')
 
 
 """
@@ -158,7 +159,7 @@ def my_filter(iterable, function):
     :param function: функция фильтрации
     :return: новая отфильтрованная последовательность
     """
-    pass
+    return [function(x) for x in iterable]
 
 
 print(my_filter([1, 2, 3, 4, 5], lambda x: x > 3) == [4, 5])  # True
